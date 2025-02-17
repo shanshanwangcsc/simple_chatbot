5a8f82a23138727c1868c6a8fbce881366c8f315
Erika Sternad <erika.sternad@csc.fi>
2025-02-17 14:19:14 +0200

    removed model token from bash script

--- run_poro_inference.sh
+++ run_poro_inference.sh
@@ -12,7 +12,7 @@ echo "Run ingest.py"
 python ingest.py
 
 # Set token as env variable
-export PORO_ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6ImF0K2p3dCJ9.eyJqdGkiOiJhYmVlNjc0YWU5ZTY0MDk2YTY1OGI4OWQ5NjdmY2VlNSIsImlzcyI6Imh0dHBzOi8vYXBpLWRldi1haXR0YS4yLnJhaHRpYXBwLmZpIiwic3ViIjoiSFNaMlVVT1Y1WFM2N0pET0ZOVlJYS09HTDNTTTdHRjMiLCJhdWQiOlsiaHR0cHM6Ly9hcGktZGV2LWFpdHRhLjIucmFodGlhcHAuZmkiXSwiZXhwIjoxNzQ3MDM2OTI2LCJzY29wZXMiOlsibW9kZWw6THVtaU9wZW4vUG9yby0zNEItY2hhdCIsIkhTWjJVVU9WNVhTNjdKRE9GTlZSWEtPR0wzU003R0YzIl19.SLVfqDDcEkJMvFMClT4nUz7-NkrNHriQ0bYwPOFMcS0"
+export PORO_ACCESS_TOKEN="<insert_token_here>"
 
 # Now we need different version for aitta-client :)
 pip install --upgrade pydantic
