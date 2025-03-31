d620022c83caaa1292a49a210f3b666cb3a50d32
Erika Sternad <erika.sternad@csc.fi>
2025-03-31 15:32:03 +0300

    Integrated Aitta/Poro for embeddings

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ Changes:
 - using Aitta for inference instead of OpenAI
 - Embeddings handled with SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2") insted of OpenAI, since Aitta does not support embedding requests yet
 - Trimmed the chat UI heavily since agent only uses db
+31-3-2025: Embeddings through Aitta
 """
 
 import os
 
 from aitta_client import Model, Client, StaticAccessTokenSource
 from langchain_community.chat_models import ChatOpenAI
+import openai
+
+from chromadb import Documents, EmbeddingFunction, Embeddings
 
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_chroma import Chroma
@@ -23,30 +27,39 @@ import chainlit as cl
 #load_dotenv()
 
 # Set up Aitta client (https://dev-aitta.2.rahtiapp.fi/page/client_guide)
-poro_access_token = os.getenv("PORO_ACCESS_TOKEN") # Get model specific token 
+poro_access_token = os.getenv("PORO_ACCESS_TOKEN") # Get model specific token
 token_source = StaticAccessTokenSource(poro_access_token)
 client = Client("https://api-dev-aitta.2.rahtiapp.fi", token_source)
 
+# Set up Poro chat model
 model = Model.load("LumiOpen/Poro-34B-chat", client)
-print(model.id)
-print(model.openai_api_url)
-
 llm = ChatOpenAI(
     temperature=0,
-    model_name=model.id,  
-    openai_api_key=token_source.get_access_token(), 
+    model_name=model.id,
+    openai_api_key=token_source.get_access_token(),
     base_url=model.openai_api_url,
-    streaming=False 
+    streaming=False
 )
 
-"""
-# Testing Poro
-response = llm.invoke("Kerro vitsi.")
-print(response)"""
+# Set up Aitta embeddings
+embed_client = openai.OpenAI(api_key=token_source.get_access_token(), base_url=model.openai_api_url)
 
-embedding_function = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
+class AittaEmbeddings(EmbeddingFunction):
+    def __init__(self, client, model_id):
+        self.client = embed_client
+        self.model_id = model_id
+
+    def embed_query(self, query: str) -> Embeddings:
+        """Create embedding for a single query"""
+        response = self.client.embeddings.create(
+            input=[query],
+            model=self.model_id
+        )
+        return response.data[0].embedding
+
+embedding_function = AittaEmbeddings(client, model.id)
 persist_directory = './db'
-vectordb = Chroma(persist_directory=persist_directory, 
+vectordb = Chroma(persist_directory=persist_directory,
                   embedding_function=embedding_function)
 
 # Set up Chainlit
@@ -76,11 +89,11 @@ async def main(message: cl.Message):
     agent_conversational = cl.user_session.get('agent_conversational')
     cb = cl.AsyncLangchainCallbackHandler()
     res = await agent_conversational.acall(message.content, callbacks=[cb])
-    
+
     answer = res["answer"]
     source_documents = res["source_documents"]
     text_elements = []
-    
+
     if source_documents:
         for source_idx, source_doc in enumerate(source_documents):
             source_name = f"source_{source_idx}"
@@ -89,5 +102,5 @@ async def main(message: cl.Message):
             )
         source_names = [text_el.name for text_el in text_elements]
         answer += f"\nSources: {', '.join(source_names)}" if source_names else "\nNo sources found"
-    
+
     await cl.Message(content=answer, elements=text_elements).send()
--- ingest.py
+++ ingest.py
@@ -1,24 +1,46 @@
 """
 Changes:
 - Embeddings handled with SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2") instead of OpenAI
-- Only .pdf files 
+- Only .pdf files
+31-3-2025: Embeddings through Aitta
 """
 
 import os
+from aitta_client import Model, Client, StaticAccessTokenSource
+import openai
+from chromadb import Documents, EmbeddingFunction, Embeddings
 
-from dotenv import load_dotenv
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-from langchain_community.embeddings import SentenceTransformerEmbeddings
 
 #from dotenv import load_dotenv
 #load_dotenv()
 
-embedding_function = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
-
 documents = []
 relative_path = './docs/'
+
+poro_access_token = os.getenv("PORO_ACCESS_TOKEN")
+token_source = StaticAccessTokenSource(poro_access_token)
+aitta_client = Client("https://api-dev-aitta.2.rahtiapp.fi", token_source)
+
+model = Model.load("LumiOpen/Poro-34B-chat", aitta_client)
+client = openai.OpenAI(api_key=token_source.get_access_token(), base_url=model.openai_api_url)
+
+class AittaEmbeddings(EmbeddingFunction):
+    def __init__(self, client, model_id):
+        self.client = client
+        self.model_id = model_id
+
+    def embed_documents(self, texts: Documents) -> Embeddings:
+        """Embed a list of documents."""
+        response = self.client.embeddings.create(
+            input=texts,
+            model=self.model_id
+        )
+        return [data.embedding for data in response.data]
+
+embedding_function = AittaEmbeddings(client, model.id)
 
 for file in os.listdir(relative_path):
     if file.endswith(".pdf"):
@@ -35,7 +57,9 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 split_documents = text_splitter.split_documents(documents)
 
-vectordb = Chroma.from_documents(split_documents, embedding=embedding_function, persist_directory="./db")
+persist_directory = "./db"
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding_function)
+vectordb.add_documents(split_documents)
 
 print("Finished creating the chromadb.")
 
